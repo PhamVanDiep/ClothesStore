f70d51bdb8d024cb41d4ccf5f0900957867a20fa
dnh1704 <huanhuanhuank56a1@gmail.com>
2022-06-14 13:25:04 +0700

    add search product in homepage

---
+++ app/views/home/search_product.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="../../../public/css/root.css" />
+    <link rel="stylesheet" href="../../../public/css/header.css" />
+    <link rel="stylesheet" href="../../../public/css/footer.css" />
+    <link rel="stylesheet" href="../../../public/css/body.css" />
+    <link rel="stylesheet" href="../../../public/css/homepage/search_product.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <?php
+    require '../components/header.php';
+    ?>
+
+    <?php
+    require '../components/footer.php';
+    ?>
+
+
+</body>
+
+</html>
