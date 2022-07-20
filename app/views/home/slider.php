c8207644ee6c382d271098b76951434ac1833464
Diep Pham Van <phamdiepa1k55@gmail.com>
2022-07-20 21:03:36 +0700

    update homepage slider and product detail color

--- app/views/home/slider.php
+++ app/views/home/slider.php
@@ -30,3 +30,13 @@
     </div>
     <!--manual navigation end-->
 </div>
+<script>
+    let counter = 1;
+    setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+    }, 5000);
+</script>
