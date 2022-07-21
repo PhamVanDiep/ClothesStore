11924bc270774869fbb81c1ae1a8a7f7da9a8812
Diep Pham Van <phamdiepa1k55@gmail.com>
2022-07-21 11:30:41 +0700

    del all

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    define("ROOT", dirname(dirname(__FILE__)));
-    define("URL",' http://localhost/web/ClothesStore');
-    define("DB_HOST", 'localhost');
-    define("DB_USER",'root');
-    define("DB_PASS",'');
-    define("DB_NAME",'market');
